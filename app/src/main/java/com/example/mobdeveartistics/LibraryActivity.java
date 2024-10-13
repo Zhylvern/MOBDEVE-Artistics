7550be46372ed9ed750b5d8aaef3b729f35237ac
Maxine Feliciano <a.maxine.feliciano@gmail.com>
2024-10-14 05:07:15 +0800

    library initialized

---
+++ app/src/main/java/com/example/mobdeveartistics/LibraryActivity.java
@@ -0,0 +1,79 @@
+package com.example.mobdeveartistics;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LibraryActivity extends AppCompatActivity {
+
+    private ConstraintLayout likedTracks, playlists, following, followers, yourUploads;
+    private ConstraintLayout song1, song2, song3;
+    private TextView btnSeeAll;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_library);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Initialize views
+        likedTracks = findViewById(R.id.libCat_likedTracks);
+        playlists = findViewById(R.id.libCat_playlists);
+        following = findViewById(R.id.libCat_following);
+        followers = findViewById(R.id.libCat_followers);
+        yourUploads = findViewById(R.id.libCat_yourUploads);
+        song1 = findViewById(R.id.hist_song1);
+        song2 = findViewById(R.id.hist_song2);
+        song3 = findViewById(R.id.hist_song3);
+        btnSeeAll = findViewById(R.id.lib_btnSeeAll);
+    }
+
+    public void likedTracksClicked(View v) {
+        Toast toast = Toast.makeText(LibraryActivity.this, "Liked tracks clicked!", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void playlistsClicked(View v) {
+        Toast toast = Toast.makeText(LibraryActivity.this, "Playlists clicked!", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void followingClicked(View v) {
+        Toast toast = Toast.makeText(LibraryActivity.this, "Following clicked!", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void followersClicked(View v) {
+        Toast toast = Toast.makeText(LibraryActivity.this, "Followers clicked!", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void yourUploadsClicked(View v) {
+        Toast toast = Toast.makeText(LibraryActivity.this, "Your uploads clicked!", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void songClicked(View v) {
+        Toast toast = Toast.makeText(LibraryActivity.this, "Song X clicked!", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void seeAllClicked(View v) {
+        Toast toast = Toast.makeText(LibraryActivity.this, "See all clicked!", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
