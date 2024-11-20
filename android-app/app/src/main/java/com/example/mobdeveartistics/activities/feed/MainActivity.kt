e9019b9ab4717634d0edadf651c5045bb83238d2
Nathan Recto <nathanrecto123@gmail.com>
2024-11-20 23:50:27 +0800

    Added temporary login button

--- android-app/app/src/main/java/com/example/mobdeveartistics/activities/feed/MainActivity.kt
+++ android-app/app/src/main/java/com/example/mobdeveartistics/activities/feed/MainActivity.kt
@@ -2,27 +2,16 @@ package com.example.mobdeveartistics.activities.feed
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mobdeveartistics.R
 import com.example.mobdeveartistics.activities.library.LibraryActivity
+import com.example.mobdeveartistics.activities.login.LoginActivity
 import com.example.mobdeveartistics.activities.profile.ProfileActivity
 import com.example.mobdeveartistics.adapters.FeedAdapter
 import com.example.mobdeveartistics.models.DataGenerator
-import io.github.jan.supabase.createSupabaseClient
-import io.github.jan.supabase.postgrest.Postgrest
-import io.github.jan.supabase.postgrest.from
-
-val supabase = createSupabaseClient(
-    supabaseUrl = "https://svqrgqcyrqqqgaofpdpa.supabase.co",
-    supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN2cXJncWN5cnFxcWdhb2ZwZHBhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE2MzYzNTMsImV4cCI6MjA0NzIxMjM1M30.LfCcg_UkJa0FQQ9LceGSFajuOmGG-XhbN_uPiEpFM7o"
-) {
-    // install(Auth)
-    install(Postgrest)
-}
 
 class MainActivity : AppCompatActivity() {
     private var recyclerView: RecyclerView? = null
@@ -59,6 +48,12 @@ class MainActivity : AppCompatActivity() {
 
     fun nav_profile_button(v: View?) {
         val i = Intent(applicationContext, ProfileActivity::class.java)
+        startActivity(i)
+    }
+
+    // Temp Login Button
+    fun navLoginButton(v: View?) {
+        val i = Intent(applicationContext, LoginActivity::class.java)
         startActivity(i)
     }
 }
